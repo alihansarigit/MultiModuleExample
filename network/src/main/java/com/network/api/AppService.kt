249a704a4cb551dbdfb8cb33704cd6d6056812fc
Alihan Sarıbaş <alihansaribas18@gmail.com>
2022-02-16 21:44:51 +0300

    IMP -> Parantez hatası giderildi

--- network/src/main/java/com/network/api/AppService.kt
+++ network/src/main/java/com/network/api/AppService.kt
@@ -5,14 +5,11 @@ import retrofit2.Response
 
 class AppService private constructor() :EndpointInterface {
 
-   
-    companion object{
         private var appService:AppService = AppService()
 
         fun getInstance():AppService{
             return appService
         }
-    }
     override suspend fun getUsers(): Users {
         return RetrofitClient.retrofitInstance.getUsers()
     }
